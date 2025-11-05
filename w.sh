49920a87e668372132ae9b24c7c91e8f57fd3013
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-05 16:31:24 +0530

    by me

--- w.sh
+++ w.sh
@@ -2,10 +2,25 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-
- echo "ERROR: You must have sudo access to run this script"
- exit 1
+  echo "ERROR: You must have sudo access to run this script"
+  exit 1
+else
+  echo "Proceeding further..."
 fi
 apt update -y
-apt install mysqlll
+apt install mysql-server -y
+if [ $? -ne 0 ]
+then
+  echo "Installing MySQL... failure"
+  exit 1
+else
+  echo "Installing MySQL... success"
+fi
 apt install nginx -y
+if [ $? -ne 0 ]
+then
+  echo "Installing NGINX... failure"
+  exit 1
+else
+  echo "Installing NGINX... success"
+fi
